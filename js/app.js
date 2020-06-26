88bce78698af8784827941f733cd862d8f7255e6
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-06-26 16:17:25 +0200

    feat: Add functionality to distinguish the section
    Add functionality to distinguish the section in view. While
    navigating through the page, the section that is active
    in the viewport should be distinguished from the other sections.
    "Active" means it takes up the most space of viewport.

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@
  * Define Global Variables
  *
  */
-
+let activeSection = document.querySelector(".your-active-class");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -33,6 +33,34 @@ function isElementVisible(element) {
   } else {
     return true;
   }
+}
+
+function getVisibilityValue(element) {
+  const details = element.getBoundingClientRect();
+  const height = Math.max(window.innerHeight || 0, document.documentElement.clientHeight || 0);
+  if (details.top < 0) {
+    return details.bottom;
+  } else {
+    if (details.bottom < height) {
+      return details.bottom - details.top;
+    } else {
+      return height - details.top;
+    }
+  }
+}
+
+function findMostVisibleElement(elements) {
+  let mostVisible = elements[0];
+  let visibilityValue = getVisibilityValue(mostVisible);
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const currVisibilityValue = getVisibilityValue(element);
+    if (currVisibilityValue > visibilityValue) {
+      mostVisible = element;
+      visibilityValue = currVisibilityValue;
+    }
+  }
+  return mostVisible;
 }
 /**
  * End Helper Functions
@@ -58,7 +86,25 @@ function buildTheNav() {
 }
 
 // Add class 'active' to section when near top of viewport
-
+function active() {
+  window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const visibles = [];
+    for (const section of sections) {
+      if (isElementVisible(section)) {
+        visibles.push(section);
+      }
+    }
+    if (visibles.length > 0) {
+      const mostVisible = findMostVisibleElement(visibles);
+      if (mostVisible !== activeSection) {
+        activeSection.classList.toggle("your-active-class");
+        mostVisible.classList.toggle("your-active-class");
+        activeSection = mostVisible;
+      }
+    }
+  });
+}
 // Scroll to anchor ID using scrollTO event
 
 /**
@@ -72,3 +118,4 @@ buildTheNav();
 // Scroll to section on link click
 
 // Set sections as active
+active();
